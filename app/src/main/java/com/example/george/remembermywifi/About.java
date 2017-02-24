9c415403ff45c00c8439abfcaf0fe19859a479f7
georgemakrakis <georgemakrakis88@gmail.com>
2017-02-24 22:02:01 +0200

    About section added

---
+++ app/src/main/java/com/example/george/remembermywifi/About.java
@@ -0,0 +1,23 @@
+package com.example.george.remembermywifi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class About extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+    public void repoPageClick(View v)
+    {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/georgemakrakis/Remember-the-WiFi-"));
+        startActivity(browserIntent);
+    }
+}
--- app/src/main/java/com/example/george/remembermywifi/MainActivity.java
+++ app/src/main/java/com/example/george/remembermywifi/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.george.remembermywifi;
 
 import android.app.ActionBar;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -40,7 +44,66 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().setTitle(Html.fromHtml("<font color='#000000'>Remember the WiFi?</font>"));
         loadList();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //When Product action item is clicked
+        if (id == R.id.rate)
+        {
 
+            Uri uri = Uri.parse("market://details?id=" + this.getPackageName());
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            try
+            {
+                startActivity(goToMarket);
+            }
+            catch (ActivityNotFoundException e)
+            {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + this.getPackageName())));
+            }
+
+        }
+        //When Contact action item is clicked
+        else if (id == R.id.contact)
+        {
+
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+            i.putExtra(Intent.EXTRA_EMAIL, new String[]{"georgemakrakis88@gmail.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, "AegeanApp_Feedback");
+            try
+            {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            }
+            catch (android.content.ActivityNotFoundException ex)
+            {
+                Toast.makeText(this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        //When About action item is clicked
+        else if (id == R.id.about)
+        {
+            Intent i = new Intent(this,About.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
     public void refreshClick(View v)
     {
         //alternative method to refresh page
